5b42511ceee89ffe26b2dd67925c044f224c2f4f
Runjeet <runjeetnarula@berkeley.edu>
2023-07-22 23:42:18 -0700

    db me please

--- pages/1_📝_Sign_Up.py
+++ pages/1_📝_Sign_Up.py
@@ -1,7 +1,23 @@
 import streamlit as st
 import User
-def storeData(name, number, email, org):
-    user = User(name, number, email, org)
+import mysql.connector
+
+# establish connection to database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Netflix9722!",
+    database="mydb"
+)
+
+myCursor = mydb.cursor()
+
+def storeData(name, number, email, password, org):
+    sql= "insert into users(name,number,email,password,org) values(%s,%s,%s,%s,%s)"
+    val= (name,number,email,password,org)
+    myCursor.execute(sql,val)
+    mydb.commit()
+    st.success("Record Created Successfully!!!")
 
 titleCol, formCol = st.columns([0.3, 0.7])
 
@@ -17,9 +33,10 @@ with formCol:
         name = st.text_input(label="Name", placeholder="Johnny Sins")
         number = st.text_input(label="Phone", placeholder="(999) 999-9999")
         email = st.text_input(label="Email", placeholder="AndrewTate@HustlersUniversity.edu")
+        password = st.text_input(label="Password", placeholder="pass123...")
         org = st.text_input(label="Fraternity/Sorority", placeholder="N/A")
         
-        submitBtn = st.form_submit_button(label="Sign Up", on_click=storeData() disabled=False, use_container_width=True) #TODO: SETUP STORE DATA FUNCTION TO CREATE AND STORE USER OBJECT
+        submitBtn = st.form_submit_button(label="Sign Up", on_click=storeData(name, number, email, password, org), disabled=False, use_container_width=True) #TODO: SETUP STORE DATA FUNCTION TO CREATE AND STORE USER OBJECT
 
 
 
